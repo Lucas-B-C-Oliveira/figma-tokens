d7e6f20d1c2d1a5b5b1e80a61f6ec19c21e184ae
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2023-02-15 15:53:16 -0400

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 15 Feb 2023 19:49:57 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
