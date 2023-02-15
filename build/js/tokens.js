1c0a595613f9f64ab082144a35d73f25ffdce6f5
Lucas-B-C-Oliveira <Lucas-B-C-Oliveira@users.noreply.github.com>
2023-02-15 21:03:00 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,9 +1,12 @@
 /**
  * Do not edit directly
- * Generated on Wed, 15 Feb 2023 19:49:57 GMT
+ * Generated on Wed, 15 Feb 2023 21:03:00 GMT
  */
 
 module.exports = {
+  "Sizes2": 9.55,
+  "Sizes4": 19.1,
+  "Sizes8": 38.2,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
